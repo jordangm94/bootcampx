8f0ba3e393b4dd0bb152157aca46aaa114ce05e5
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-20 19:49:45 +0000

    Added protection against SQL injection USING query params and query string, and passing these into pool.query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -25,23 +25,27 @@ const pool = new Pool({
 //our console string.
 
 //Store what would be limit in a variable, forth entry into terminal
-const limit = process.argv.slice(3);
+const limit = process.argv.slice(3)[0];
 
 //Store what would be DATE in a variable, third entry into terminal. Slice at 2, will give [FEB, 12], therefore target array index 0 as well. 
 const DATE = process.argv.slice(2)[0];
 
-pool
-  .query(
-    //Input our variables with our terminal input into our SQL query
-    //LIKE statements looks for something THAT starts with date inputted
-    `
+
+console.log(limit);
+//Values that will be inserted by user will be stored in an array within availabe. 
+const queryParams = [`${DATE}%`, limit ]
+
+const queryString =     `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${DATE}%'
-LIMIT ${limit};
-`
-  )
+WHERE cohorts.name LIKE $1
+LIMIT $2`
+
+pool.query(
+    //Input our variables with our terminal input into our SQL query
+    //LIKE statements looks for something THAT starts with date inputted
+queryString, queryParams)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,23 +8,30 @@ const pool = new Pool({
 });
 
 //Slice out node and teachers.js in input and store third input in variable. 
-const cohortDATE = process.argv.slice(2);
+const cohortDATE = process.argv.slice(2)[0];
+//EXTREMELY IMPORTANT NOTE: This value is stored inside cohortdate, which is passed to query params,
+//If you do not target the value specifically, i.e. including [0], slice, will give you an array with value.
+//This will mean inside query params you will have array, with array inside that contains yur value. 
+//Query does not like this and will stop your code from working. Keep this in mind! 
+
+//Store information inserted by user into queryParams
+const queryParams = [cohortDATE]
+
+const queryString =     `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teachers.id=teacher_id 
+  JOIN students ON student_id=students.id
+  JOIN cohorts ON cohort_id=cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher`
 
 //Incorporate this input into the SQL statement,so our cohorts.name will be what we input. 
 pool
   .query(
     //Input our variables with our terminal input into our SQL query
     //LIKE statements looks for something THAT starts with date inputted
-    `
-    SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-    FROM teachers
-    JOIN assistance_requests ON teachers.id=teacher_id 
-    JOIN students ON student_id=students.id
-    JOIN cohorts ON cohort_id=cohorts.id
-    WHERE cohorts.name = '${cohortDATE}'
-    ORDER BY teacher;
-`
-  )
+    queryString, queryParams)
   //Adjust promise statement accordingly to tackle and print information accordingly
   //Notice how teacher is object in paranthesis, and we call upon that object using the aliases from SQL!
   .then((res) => {
